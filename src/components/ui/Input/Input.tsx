72db8180678e4895d1ac7cdb5baa2dba0f98b8df
jueji3512 <1722772741@qq.com>
2025-02-15 18:19:40 +0800

    feat(components): 搭建了最基本的input公用组件

---
+++ src/components/ui/Input/Input.tsx
@@ -0,0 +1,24 @@
+'use client';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
+
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export const Input = ({ className, ...props }: InputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const inputWrapperClasses = cn(
+    'flex border border-gray-300 rounded-lg transition-all duration-300',
+    isFocused && 'outline-1 outline-gray-500 border-gray-500',
+  );
+  const inputClasses = cn('w-full rounded-md px-2 py-1 text-sm outline-none', className);
+  return (
+    <div className={inputWrapperClasses}>
+      <input
+        type="text"
+        className={inputClasses}
+        {...props}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+      />
+    </div>
+  );
+};
